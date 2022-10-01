74d901758522149272bdb36c8383be2c5cd8e8ea
Sabin Bhandari <sabi5403@edu.easj.dk>
2022-10-01 15:44:52 +0200

    Add project files.

---
+++ MyOwnFC/FootballPlayer.cs
@@ -0,0 +1,53 @@
+﻿namespace MyOwnFC
+{
+    public class FootballPlayer
+    {
+
+        public int Id { set; get; }
+        public string? Name { set; get; }
+        public int Age { set; get; }
+        public int ShirtNumber { get; set; }
+
+        public FootballPlayer(int id, string name, int age, int shirtNumber)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+            ShirtNumber= shirtNumber;
+        }
+
+   
+
+        public void ValidateName()
+        {
+            //at least 2 characters
+            if (Name?.Length < 2)
+            {
+                throw new ArgumentException("Player name too short");
+            }
+        }
+        public void ValidateAge()
+        {
+            //age > 1
+            if (Age<= 1)
+            {
+                throw new ArgumentOutOfRangeException("Player age must be greater than one");
+            }
+        }
+        public void ValidateShirtNumber()
+        {
+            // number, 1 <= ShirtNumber <= 99
+            if (ShirtNumber > 99 || ShirtNumber < 1 )
+            {
+                throw new ArgumentOutOfRangeException(" Shirt Number should be between 1 and  99");
+            }
+        }
+        public void Validate()
+        {
+            ValidateName();
+            ValidateShirtNumber();
+            ValidateAge();
+
+        }
+    }
+}
---
+++ MyOwnFCTests/FootballPlayerTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyOwnFC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyOwnFC.Tests
+{
+    [TestClass()]
+    public class FootballPlayerTests
+    {
+        
+           FootballPlayer playerTitleOneWord = new FootballPlayer(1, "R", 17 , 29 );
+           FootballPlayer player = new FootballPlayer(2, "David", 1, 10);
+           FootballPlayer player2 = new FootballPlayer(3, "Lucy", 16, 100);
+
+        
+
+        [TestMethod()]
+
+        
+        public void ValidateNameTest()
+        {
+            // Arrange
+
+            //Act
+
+      
+
+            //Assert
+
+            Assert.ThrowsException<ArgumentException>(() => playerTitleOneWord.ValidateName());
+        }
+        [TestMethod()]
+        public void ValidateAge()
+        {
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => player.ValidateAge());
+        }
+        [TestMethod()]
+        public void ValidateShirtNumber()
+        {
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => player2.ValidateShirtNumber());
+        }
+    }
+}
